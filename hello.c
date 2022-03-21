c36552dc159e90f9fd7266b174cfad7c48049b7d
TomasGoyo <101760715+TomasGoyo@users.noreply.github.com>
2022-03-21 21:02:18 +0100

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (void) {
+    char nombre [15];
+    printf("buenas tardes, ingrese su nombre ");
+    scanf("%s",nombre);
+    printf("hola %s\n",nombre);
+    return 0;
+}
